14c4eddbd5bce285a30de5e085b0dc8bdf41c8e2
Artem Abramov <ar.abramov@gmail.com>
2017-02-19 15:49:15 +0200

    Initial Commit

---
+++ HubchatTestProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HubchatTestProject
+//
+//  Created by Artem Abramov on 19/02/2017.
+//  Copyright © 2017 Artem Abramov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
